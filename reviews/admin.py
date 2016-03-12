2888ebac3fe4db82042bf9bc5e747855e612062b
jerodestapa <jerodestapa@gmail.com>
2016-03-12 16:53:10 -0600

    Added models to admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Beer, Review
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    model = Review
+    list_display = ('beer', 'rating', 'user_name', 'comment', 'pub_date')
+    list_filter = ['pub_date', 'user_name']
+    search_fields = ['comment']
+
+admin.site.register(Beer)
+admin.site.register(Review, ReviewAdmin)
