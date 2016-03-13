db34d943acda1dc5c8416a23b37bb361e146c487
jerodestapa <jerodestapa@gmail.com>
2016-03-13 13:15:53 -0500

    Added reviews app urls and views

--- reviews/models.py
+++ reviews/models.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 class Beer(models.Model):
-    name=models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
 
     def average_rating(self):
         all_ratings = map(lambda x: x.rating, self.review_set.all())
@@ -23,7 +23,7 @@ class Review(models.Model):
         (4, '4'),
         (5, '5'),
     )
-    wine = models.ForeignKey(Beer)
+    beer = models.ForeignKey(Beer)
     pub_date = models.DateTimeField('date published')
     user_name = models.CharField(max_length=100)
     comment = models.CharField(max_length=200)
